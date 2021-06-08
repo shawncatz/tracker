1edb428c8e8580bd1709fef5b301351dadaf7854
Shawn Catanzarite <me@shawncatz.com>
2021-06-07 21:21:02 -0700

    just do something basic

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import (
+	"github.com/docopt/docopt-go"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	usage := `Tracker
+
+Usage:
+  tracker setup
+  tracker new <name>
+  tracker remove <name>
+  tracker add <name> <value>
+  tracker list <name>
+  tracker -h | --help
+  tracker --version
+
+Options:
+  -h --help			Show this screen.
+  --version			Show version.
+`
+	args, err := docopt.ParseDoc(usage)
+	if err != nil {
+		logrus.Errorf("error: %s", err)
+	}
+
+	logrus.Debugf("args: %#v", args)
+}
